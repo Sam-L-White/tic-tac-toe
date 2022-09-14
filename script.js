353aabc0a0e8dac461ce17b2da086f05ac19065e
Sam White <samuelwhite98@outlook.com>
2022-09-14 19:24:54 +0100

    Add logic to grab board array and display in grid

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const gameBoard = (() => {
     let boardArray = [
-        ["X", "X", "X"],
-        ["X", "O", "X"],
-        ["X", "X", "X"]
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"]
     ];
-    let displayArray = () => console.log(boardArray);
-    
-    return {displayArray}
+    let boardValue = (x,y) => {
+        return boardArray[x][y]
+    }
+
+    return {boardValue}
 })();
 
 const player = () => {
@@ -14,6 +16,16 @@ const player = () => {
 }
 
 const displayController = (() => {
-    gameBoard.displayArray()
+    let container = document.querySelector(".gameboard")
+    for(x = 0; x<3; x++){
+
+        for(y = 0; y<3; y++){
+            console.log(gameBoard.boardValue(x,y))
+            let div = document.createElement("div")
+            div.classList.add("gridbox")
+            div.textContent = gameBoard.boardValue(x,y)
+            container.appendChild(div)
+        }
+    }
 })();
 
