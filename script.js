f60b8101bf36e903fa6873375bca3b72312cb314
Sam White <samuelwhite98@outlook.com>
2022-09-15 20:48:12 +0100

    Allow players to place markers and alternate

--- script.js
+++ script.js
@@ -1,31 +1,85 @@
 const gameBoard = (() => {
     let boardArray = [
-        ["1", "2", "3"],
-        ["4", "5", "6"],
-        ["7", "8", "9"]
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
     ];
-    let boardValue = (x,y) => {
+    const boardValue = (x,y) => {
         return boardArray[x][y]
     }
 
-    return {boardValue}
+    return {boardArray,boardValue}
 })();
 
-const player = () => {
+const player = (name,marker) => {
+    const getName = () => name;
+    const getMarker = () => marker;
 
+    const placeMarker = (div_id) => {
+        //update array with marker value
+        let coordinates = div_id.split(",")
+        let x = coordinates[0]
+        let y = coordinates[1]
+        gameBoard.boardArray[x][y] = marker
+        console.log(gameBoard.boardArray)
+        displayController.updateBoard(div_id,x,y)
+        
+    }
+    return {getName, getMarker, placeMarker}
 }
 
+const gameController = (() => {
+    let playerOne = player("One","X")
+    let playerTwo = player("Two","O")
+    let playerOneTurn = true
+    const playerTurn = (div_id) => {
+        //swap between players
+        if (playerOneTurn == true){
+            playerOne.placeMarker(div_id)
+            playerOneTurn = false
+        } else {
+            playerTwo.placeMarker(div_id)
+            playerOneTurn = true
+        }
+        
+    }
+
+    return {playerTurn}
+})();
+
 const displayController = (() => {
+
     let container = document.querySelector(".gameboard")
     for(x = 0; x<3; x++){
 
         for(y = 0; y<3; y++){
-            console.log(gameBoard.boardValue(x,y))
+            //for each array item, create a div within the gameboard container
+            
             let div = document.createElement("div")
             div.classList.add("gridbox")
+            div.id = `${x},${y}`
             div.textContent = gameBoard.boardValue(x,y)
+            if (div.textContent == ""){
+                div.addEventListener("click", function clickevent(){
+                    gameController.playerTurn(div.id)
+                })
+                console.log("added")
+            }
+            
+            
             container.appendChild(div)
         }
     }
+
+    const updateBoard = (div_id, x, y) => {
+        //apply updated array value to display, then remove event listener
+        let div = document.getElementById(div_id)
+        div.textContent = gameBoard.boardArray[x][y]
+        let new_div = div.cloneNode(true);
+        div.parentNode.replaceChild(new_div, div);
+        console.log("removed")
+    }
+
+    return {updateBoard}
 })();
 
