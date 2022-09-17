6b6ff7d1d498e8f28a5cc0d1ebb1bf1c8d2b39a9
Sam White <samuelwhite98@outlook.com>
2022-09-17 15:40:16 +0100

    Add logic for winning game, add html controls

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ const player = (name,marker) => {
         let x = coordinates[0]
         let y = coordinates[1]
         gameBoard.boardArray[x][y] = marker
-        console.log(gameBoard.boardArray)
         displayController.updateBoard(div_id,x,y)
+        gameController.checkBoard()
         
     }
     return {getName, getMarker, placeMarker}
@@ -33,6 +33,7 @@ const gameController = (() => {
     let playerTwo = player("Two","O")
     let playerOneTurn = true
     const playerTurn = (div_id) => {
+
         //swap between players
         if (playerOneTurn == true){
             playerOne.placeMarker(div_id)
@@ -40,11 +41,53 @@ const gameController = (() => {
         } else {
             playerTwo.placeMarker(div_id)
             playerOneTurn = true
-        }
-        
+        }    
     }
+    const checkBoard = () => {
+        const winCombos = [
+            ["0,0","1,0","2,0"],
+            ["0,1","1,1","2,1"],
+            ["0,2","1,2","2,2"],
+            ["0,0","0,1","0,2"],
+            ["1,0","1,1","1,2"],
+            ["2,0","2,1","2,2"],
+            ["0,0","1,1","2,2"],
+            ["0,2","1,1","2,0"]
+        ];
+        
+        
+        
+        winCombos.forEach(function(row){
+            pos1 = row[0].split(",")
+            pos2 = row[1].split(",")
+            pos3 = row[2].split(",")
+            
+            
+            
+            if(gameBoard.boardArray[pos1[0]][pos1[1]] === gameBoard.boardArray[pos2[0]][pos2[1]]){
+                if(gameBoard.boardArray[pos2[0]][pos2[1]] === gameBoard.boardArray[pos3[0]][pos3[1]]){
+                    switch(gameBoard.boardArray[pos3[0]][pos3[1]]){
+                        case(playerOne.getMarker()):
+                            console.log(`${playerOne.getName()} wins!`)
+                            break;
+                        case(playerTwo.getMarker()):
+                            console.log(`${playerTwo.getName()} wins!`)
+                            break;
+                        break;
+                    }
+                }
+            } 
 
-    return {playerTurn}
+            
+        })
+        
+    };
+        
+        
+        
+
+
+    return {playerTurn, checkBoard}
 })();
 
 const displayController = (() => {
@@ -63,7 +106,7 @@ const displayController = (() => {
                 div.addEventListener("click", function clickevent(){
                     gameController.playerTurn(div.id)
                 })
-                console.log("added")
+                
             }
             
             
@@ -77,7 +120,7 @@ const displayController = (() => {
         div.textContent = gameBoard.boardArray[x][y]
         let new_div = div.cloneNode(true);
         div.parentNode.replaceChild(new_div, div);
-        console.log("removed")
+        
     }
 
     return {updateBoard}
